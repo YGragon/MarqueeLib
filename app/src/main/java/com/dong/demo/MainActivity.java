3d6a3c3083aacd4df5bd510f46ee4e0f50f7d081
dongxi346 <1105894953@qq.com>
2018-08-27 22:27:17 +0800

    添加点击飘心测试抢占焦点

--- app/src/main/java/com/dong/demo/MainActivity.java
+++ app/src/main/java/com/dong/demo/MainActivity.java
@@ -1,7 +1,7 @@
 package com.dong.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -10,22 +10,32 @@ import com.dong.marqueelib.MarqueeTextviewNofocus;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FlowLikeView mLikeViewLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
         Button btnClick = findViewById(R.id.btn_click);
+
+        // 点击飘心控件，和跑马灯无关
+        mLikeViewLayout = findViewById(R.id.flowLikeView);
+
         MarqueeTextviewNofocus marqueeTextviewNofocus = findViewById(R.id.tv_marquee);
-        marqueeTextviewNofocus.init(getWindowManager());
-        marqueeTextviewNofocus.setScrollMode(MarqueeTextviewNofocus.SCROLL_NORM);
-        marqueeTextviewNofocus.setScrollDirection(MarqueeTextviewNofocus.RIGHT_TO_LEFT);
         marqueeTextviewNofocus.setText("大家好啊，我是跑马灯~");
-        marqueeTextviewNofocus.setTextColor(getResources().getColor(R.color.colorAccent));
+        // 初始化
+        marqueeTextviewNofocus.init(getWindowManager());
+        // 设置滚动方向
+        marqueeTextviewNofocus.setScrollDirection(MarqueeTextviewNofocus.RIGHT_TO_LEFT);
+        // 设置滚动速度
+        marqueeTextviewNofocus.setScrollMode(MarqueeTextviewNofocus.SCROLL_FAST);
+
         btnClick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "点击按钮，获取焦点~", Toast.LENGTH_SHORT).show();
+                mLikeViewLayout.addLikeView();
+                Toast.makeText(MainActivity.this, "我不要焦点~", Toast.LENGTH_SHORT).show();
             }
         });
     }
